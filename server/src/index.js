7b2c60cb368bf7cde0df81664f30e677e0dbf7ea
Nikodem Domaracki <56053287+Nikkoro@users.noreply.github.com>
2022-12-01 01:55:30 +0100

    feat: set up new endpoint for downloading & validate request body

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,35 @@
 import express from "express";
+import cors from "cors";
+import { exec } from "child_process";
+import { body, validationResult } from "express-validator";
 
 const PORT = 3000;
+const downloadSong = (yturl) => {
+  exec(
+    `yt-dlp -f bestaudio --extract-audio --audio-format mp3 --embed-thumbnail --add-metadata -o "%(title)s.%(ext)s" ${yturl}`,
+    { cwd: "../binaries" }
+  );
+};
+
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello WorldxD!");
+});
+
+app.post("/download", body("url").isURL(), (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const yturl = req.body.url;
+  downloadSong(yturl);
+  return res.status(200).json({ message: "Download started" });
 });
 
 app.listen(PORT, () => {

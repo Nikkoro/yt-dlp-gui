07bc17ef41ff6f75edb26a3ff7562a08bc069b55
Nikodem Domaracki <56053287+Nikkoro@users.noreply.github.com>
2022-12-01 01:55:53 +0100

    feat: connect frontend with api

--- frontend/yt-dlp-gui/src/App.jsx
+++ frontend/yt-dlp-gui/src/App.jsx
@@ -1,34 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //console.log(JSON.stringify({ url }));
+    fetchVideo(url);
+  };
+
+  const fetchVideo = async (url) => {
+    const response = await fetch("http://localhost:3000/download", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>yt-dlp mp3 downloader</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="url-input">Youtube URL: </label>
+        <input
+          id="url-input"
+          type="text"
+          value={url}
+          onChange={(e) => {
+            setUrl(e.target.value);
+          }}
+        />
+        <button>Download</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

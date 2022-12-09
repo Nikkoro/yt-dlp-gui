a973039c1039dd38d46426120eca0427b77ae8d8
Nikodem Domaracki <56053287+Nikkoro@users.noreply.github.com>
2022-12-09 17:11:13 +0100

    feat: add new endpoint for getting title and thumbnail

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,39 @@
 import express from "express";
 import cors from "cors";
-import { exec } from "child_process";
 import { body, validationResult } from "express-validator";
 
 const PORT = 3000;
+import { spawn } from "child_process";
+
 const downloadSong = (yturl) => {
-  exec(
-    `yt-dlp -f bestaudio --extract-audio --audio-format mp3 --embed-thumbnail --add-metadata -o "%(title)s.%(ext)s" ${yturl}`,
+  const download = spawn(
+    "yt-dlp",
+    [
+      "-f",
+      "bestaudio",
+      "--extract-audio",
+      "--audio-format",
+      "mp3",
+      "--embed-thumbnail",
+      "--add-metadata",
+      "-o",
+      "%(title)s.%(ext)s",
+      yturl,
+    ],
     { cwd: "../binaries" }
   );
+
+  download.stdout.on("data", (data) => {
+    console.log(`stdout: ${data}`);
+  });
+
+  download.stderr.on("data", (data) => {
+    console.error(`stderr: ${data}`);
+  });
+
+  download.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 };
 
 const app = express();
@@ -19,6 +44,45 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello WorldxD!");
+});
+
+app.post("/video", (req, res) => {
+  const youtubeUrl = req.body.url;
+  const info = spawn("yt-dlp", [
+    "--get-title",
+    "--get-thumbnail",
+    "--get-filename",
+    youtubeUrl,
+  ]);
+
+  info.stdout.on("data", (chunk) => {
+    console.log(`yt-dlp stdout: ${chunk}`);
+  });
+
+  info.stderr.on("data", (chunk) => {
+    console.error(`yt-dlp stderr: ${chunk}`);
+  });
+
+  info.on("close", (code) => {
+    console.log(`yt-dlp exited with code: ${code}`);
+  });
+
+  let data = "";
+
+  info.stdout.on("data", (chunk) => {
+    data += chunk;
+  });
+
+  info.on("close", () => {
+    const lines = data.split("\n");
+    const video = {
+      title: lines[0],
+      thumbnail: lines[1],
+      author: lines[2],
+    };
+
+    res.json(video);
+  });
 });
 
 app.post("/download", body("url").isURL(), (req, res) => {
